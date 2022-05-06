75d035e3c66aab65c05bf739585c8b4e20abbd6c
Adrien35240 <ribaultadrien@orange.fr>
2022-05-06 17:23:10 +0200

    feat: add helloworld lambda

--- deploy/main.tf
+++ deploy/main.tf
@@ -39,3 +39,62 @@ resource "aws_s3_bucket_acl" "acl_buckettodolist" {
   acl = "private"
 }
 
+# copy lambda function code in the bucket
+data "archive_file" "lambda_helloworld_tl" {
+  type = "zip"
+
+  source_dir  = "../lambdas"
+  output_path = "${path.module}/helloworld.zip"
+}
+
+resource "aws_s3_object" "lambda_helloworld_tl" {
+  bucket = aws_s3_bucket.lambda_buckettodolist.id
+
+  key    = "helloworld.zip"
+  source = data.archive_file.lambda_helloworld_tl.output_path
+
+  etag = filemd5(data.archive_file.lambda_helloworld_tl.output_path)
+}
+
+# define the lambda function
+resource "aws_lambda_function" "hello_world" {
+  function_name = "HelloWorld"
+
+  s3_bucket = aws_s3_bucket.lambda_buckettodolist.id
+  s3_key    = aws_s3_object.lambda_helloworld_tl.key
+
+  runtime = "nodejs14.x"
+  handler = "hello.handler"
+
+  source_code_hash = data.archive_file.lambda_helloworld_tl.output_base64sha256
+
+  role = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_cloudwatch_log_group" "hello_world" {
+  name = "/aws/lambda/${aws_lambda_function.hello_world.function_name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_lambda"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
--- deploy/output.tf
+++ deploy/output.tf
@@ -3,3 +3,9 @@ output "lambda_buckettodolist" {
   description = "Lambda store bucket name :"
   value = aws_s3_bucket.lambda_buckettodolist.id
 }
+# lamba function output
+output "hello_world" {
+  description = "Name of the Lambda function."
+
+  value = aws_lambda_function.hello_world.function_name
+}
